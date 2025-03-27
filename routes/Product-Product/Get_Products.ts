fe59eeb7da1fa36ab73c86d175d6c204627b2683
kevinADS420 <kevin.aguirre.ads@gmail.com>
2025-03-27 02:03:30 -0500

    Actualisacion del back

--- routes/Product-Product/Get_Products.ts
+++ routes/Product-Product/Get_Products.ts
@@ -1,9 +1,8 @@
 import express from "express";
 import getAllProductsController from "../../controllers/Product-controller/get-product-controller";
-import verifyToken from "../../middleware/VerifyToken";
 
 const router = express.Router();
 
-router.get('/', verifyToken, getAllProductsController);
+router.get('/', getAllProductsController);
 
 export default router;
